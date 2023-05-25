2877bcd499a0ccfcd3b8ca7383a3e50d558a56b5
sou_rabh <sourabhrwt8126544591@gmail.com>
2023-05-25 14:02:27 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-csd4x3qsts1h2bpg.us.auth0.com"
+      clientId="kjTnLD30k4ilQRX6P7o6l18geBJgaUHP"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+    ,
   </React.StrictMode>
 );
 
